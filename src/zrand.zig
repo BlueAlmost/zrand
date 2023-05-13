e272f1775f369f6c986b8ee366a2ccf78de023c1
BlueAlmost <git.blue.almost@gmail.com>
2023-05-13 20:57:34 +0200

    updated build and fmt for loops

--- src/zrand.zig
+++ src/zrand.zig
@@ -6,78 +6,127 @@ const Complex = std.math.complex.Complex;
 // const zrand = @import("zrand");
 
 fn ValueType(comptime T: type) type {
-    switch(T) {
+    switch (T) {
+        u8,
+        []u8,
+        => {
+            return u8;
+        },
+        u16,
+        []u16,
+        => {
+            return u16;
+        },
+        u32,
+        []u32,
+        => {
+            return u32;
+        },
+        u64,
+        []u64,
+        => {
+            return u64;
+        },
+        usize, []usize => {
+            return usize;
+        },
 
-        u8, []u8, => {return u8;},
-        u16, []u16, => {return u16;},
-        u32, []u32, => {return u32;},
-        u64, []u64, => {return u64;},
-        usize, []usize => {return usize;},
+        i8,
+        []i8,
+        => {
+            return i8;
+        },
+        i16,
+        []i16,
+        => {
+            return i16;
+        },
+        i32,
+        []i32,
+        => {
+            return i32;
+        },
+        i64,
+        []i64,
+        => {
+            return i64;
+        },
 
-        i8, []i8, => {return i8;},
-        i16, []i16, => {return i16;},
-        i32, []i32, => {return i32;},
-        i64, []i64, => {return i64;},
-
-        f32, Complex(f32) => {return f32;},
-        f64, Complex(f64) => {return f64;},
-        []f32, []Complex(f32) => {return f32;},
-        []f64, []Complex(f64) => {return f64;},
-        else => {@compileError("type not implemented");}
+        f32, Complex(f32) => {
+            return f32;
+        },
+        f64, Complex(f64) => {
+            return f64;
+        },
+        []f32, []Complex(f32) => {
+            return f32;
+        },
+        []f64, []Complex(f64) => {
+            return f64;
+        },
+        else => {
+            @compileError("type not implemented");
+        },
     }
 }
 
 fn isComplex(comptime T: type) bool {
-    switch(T) {
-        []u8, []u16, []u32, []u64, []usize, []i8, []i16, []i32, []i64 => { return false;},
-        []f32, []f64 => { return false; },
-        []Complex(f32), []Complex(f64) => { return true; },
-        else => {@compileError("type not implemented");},
+    switch (T) {
+        []u8, []u16, []u32, []u64, []usize, []i8, []i16, []i32, []i64 => {
+            return false;
+        },
+        []f32, []f64 => {
+            return false;
+        },
+        []Complex(f32), []Complex(f64) => {
+            return true;
+        },
+        else => {
+            @compileError("type not implemented");
+        },
     }
 }
 
-pub fn randn(rnd: std.rand.Random, x: anytype ) void {
-
+pub fn randn(rnd: std.rand.Random, x: anytype) void {
     const T = @TypeOf(x);
     comptime var isCmpx: bool = isComplex(T);
     comptime var V: type = ValueType(T);
 
-    var r: V      = undefined;
-    var v1: V     = undefined;
-    var v2: V     = undefined;
-    var theta: V  = undefined;
-    var i: usize  = 0;
-    var n: usize  = x.len;
+    var r: V = undefined;
+    var v1: V = undefined;
+    var v2: V = undefined;
+    var theta: V = undefined;
+    var i: usize = 0;
+    var n: usize = x.len;
 
     switch (isCmpx) {
-
         false => {
-            var n2: usize = n/2;
-            while( i<n2 ) : ( i += 1 ) {
+            var n2: usize = n / 2;
+            while (i < n2) : (i += 1) {
                 v1 = rnd.float(V);
                 v2 = rnd.float(V);
                 r = math.sqrt(-2.0 * math.ln(v1));
                 theta = 2.0 * math.pi * v2;
-                x[2*i] = r * @cos(theta);
-                x[2*i+1] = r * @sin(theta);
+                x[2 * i] = r * @cos(theta);
+                x[2 * i + 1] = r * @sin(theta);
             }
 
             // compute last point if we had odd length of x
-            if( 2 * n2 < n) {
+            if (2 * n2 < n) {
                 v1 = rnd.float(V);
                 v2 = rnd.float(V);
                 r = math.sqrt(-2.0 * math.ln(v1));
-                theta = 2.0 * math.pi*v2;
-                x[n-1] = r * @cos(theta);
+                theta = 2.0 * math.pi * v2;
+                x[n - 1] = r * @cos(theta);
             }
         },
 
         true => {
-            while( i<n ) : ( i+=1 ) {
+            while (i < n) : (i += 1) {
                 v1 = rnd.float(V);
                 v2 = rnd.float(V);
-                r = math.sqrt1_2 * math.sqrt(-2.0*math.ln(v1));
-                theta = 2.0 * math.pi*v2;
+                r = math.sqrt1_2 * math.sqrt(-2.0 * math.ln(v1));
+                theta = 2.0 * math.pi * v2;
                 x[i].re = r * @cos(theta);
                 x[i].im = r * @sin(theta);
             }
@@ -85,43 +134,44 @@ pub fn randn(rnd: std.rand.Random, x: anytype ) void {
     }
 }
 
-pub fn rand(rnd: std.rand.Random, x: anytype ) void {
-
+pub fn rand(rnd: std.rand.Random, x: anytype) void {
     const T = @TypeOf(x);
     comptime var isCmpx: bool = isComplex(T);
     comptime var V: type = ValueType(T);
 
     switch (isCmpx) {
-
         false => {
-
-            switch(T) {
+            switch (T) {
                 []u8, []u16, []u32, []u64, []i8, []i16, []i32, []i64 => {
-                    for (x) |_, i| { x[i] = rnd.int(V); }
+                    for (x, 0..) |_, i| {
+                        x[i] = rnd.int(V);
+                    }
                 },
 
                 []f32, []f64 => {
-                    for (x) |_, i| { x[i] = rnd.float(V); }
+                    for (x, 0..) |_, i| {
+                        x[i] = rnd.float(V);
+                    }
                 },
-                else => { @compileError("type is not implmented");},
+                else => {
+                    @compileError("type is not implmented");
+                },
             }
         },
 
         true => {
-            for (x) |_, i| {
+            for (x, 0..) |_, i| {
                 x[i].re = rnd.float(V);
                 x[i].im = rnd.float(V);
             }
         },
-
     }
 }
 
 //--- TESTS ----------------------------------------------------------
 
 test "\t randn \t real array\n" {
-    inline for (.{f32, f64}) |T| {
-
+    inline for (.{ f32, f64 }) |T| {
         const n = 3;
 
         var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
@@ -141,10 +191,8 @@ test "\t randn \t real array\n" {
     }
 }
 
-
 test "\t randn \t complex array\n" {
-    inline for (.{f32, f64}) |T| {
-
+    inline for (.{ f32, f64 }) |T| {
         const C = Complex(T);
 
         const n = 3;
@@ -160,8 +208,7 @@ test "\t randn \t complex array\n" {
 
         print("\n", .{});
         for (x) |xval| {
-            print("\t({e:>10.3}, {e:>10.3})\n", .{xval.re, xval.im});
+            print("\t({e:>10.3}, {e:>10.3})\n", .{ xval.re, xval.im });
         }
     }
 }
-
